5d22d2005c49c3a6d2765e84eea5e66b41af85f5
mattia.mazzon <mattia.mazzon@oncode.it>
2018-07-13 08:28:05 +0200

    [MM] Initial commit

---
+++ SimpleTCPClient/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(59, 75)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(126, 42)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Prova"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(25, 27)
+        Me.TextBox1.MaxLength = 3
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(74, 27)
+        Me.TextBox2.MaxLength = 3
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(123, 27)
+        Me.TextBox3.MaxLength = 3
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(172, 27)
+        Me.TextBox4.MaxLength = 3
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(43, 20)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(25, 51)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.ReadOnly = True
+        Me.TextBox5.Size = New System.Drawing.Size(190, 20)
+        Me.TextBox5.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(245, 131)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Simple TCP/IP Client"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+End Class
---
+++ SimpleTCPClient/Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports System.Net.Sockets
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Dim buff() As Byte = New Byte() {0, 2, 3, 4, 5} ' The first byte is ignored, the second is the ASCII code for STX (Start Of Text)
+        Dim buff() As Byte = New Byte() {67, 105, 97, 111}
+        Dim message() As Byte
+        message = sendMessage(buff)
+        TextBox5.ResetText()
+        TextBox5.Text = UnicodeBytesToString(message)
+    End Sub
+
+    Private Function UnicodeStringToBytes(ByVal str As String) As Byte()
+        Return System.Text.Encoding.Unicode.GetBytes(str)
+    End Function
+
+    Private Function UnicodeBytesToString(ByVal byt() As Byte) As String
+        Return System.Text.Encoding.Default.GetString(byt)
+    End Function
+
+    Private Function CheckAddress() As Boolean
+        If TextBox1.Text.Trim() = "" Or TextBox2.Text.Trim() = "" Or TextBox3.Text.Trim() = "" Or TextBox4.Text.Trim = "" Then
+            MessageBox.Show("The IP address fields are not all correctly compiled. The IP address must be in the form: XXX.XXX.XXX.XXX where XXX are a number between 0 and 255")
+            Return False
+        End If
+        Return True
+    End Function
+
+    Private Function sendMessage(messageBytes As Byte()) As Byte()
+        ' Check if a valid IP address is inserted
+        If Not CheckAddress() Then Exit Function
+
+        Const bytesize As Integer = 1024 * 1024
+        Try
+            ' Reconstruct the IP address as string
+            Dim ipAddress As String = TextBox1.Text + "." + TextBox2.Text + "." + TextBox3.Text + "." + TextBox4.Text
+
+            ' Try connecting and send the message bytes  
+            Dim client As New TcpClient(ipAddress, 11100)
+
+            ' Create a new connection  
+            Dim stream As NetworkStream = client.GetStream()
+
+            stream.Write(messageBytes, 0, messageBytes.Length)
+
+            ' Clear the message
+            messageBytes = New Byte(bytesize - 1) {}
+
+            ' Receive the stream of bytes  
+            stream.Read(messageBytes, 0, messageBytes.Length)
+
+            ' If I've correctly received the end of transmission byte EOT (ASCII char 4)
+            For Each character As Byte In messageBytes
+                If character = 4 Then
+                    stream.Close()
+                    'stream.Dispose()
+                    client.Close()
+                End If
+            Next
+
+        Catch e As Exception
+            ' Catch exceptions  
+            MsgBox(e.Message)
+        End Try
+
+        Return messageBytes
+        ' Return response  
+    End Function
+
+    Private Sub TextBoxes_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress, TextBox2.KeyPress, TextBox3.KeyPress, TextBox4.KeyPress
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                If Asc(e.KeyChar) = 46 Then
+                    SendKeys.Send("{TAB}")
+                End If
+                e.Handled = True
+            End If
+        End If
+    End Sub
+End Class
